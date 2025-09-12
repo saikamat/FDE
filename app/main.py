b4a94a5676402118bf227f399d55f1f40f251f0c
saikamat <sai.s.kamat@gmail.com>
2025-09-12 17:31:37 +0200

    serving frontend

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 # It imports FastAPI for building the API and sqlite3 for interacting with a SQLite database.
 from fastapi import FastAPI
 import sqlite3
+from fastapi.responses import FileResponse
+import os
 
 # initializes the FastAPI app
 app = FastAPI()
+
+@app.get("/")
+def root():
+    return FileResponse(os.path.join(os.path.dirname(__file__), "..", "index.html"))
 
 # /hello endpoint accepts a query parameter name (default is "World").
 @app.get("/hello")
